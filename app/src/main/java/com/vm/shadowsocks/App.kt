ce9a07bc78015a9251aa26f1ca67a0f799031686
sand <sand@cretve.com>
2018-05-10 12:29:41 +0800

    add user register

--- app/src/main/java/com/vm/shadowsocks/App.kt
+++ app/src/main/java/com/vm/shadowsocks/App.kt
@@ -5,15 +5,17 @@ import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.support.multidex.MultiDex
 import android.support.multidex.MultiDexApplication
-import android.text.TextUtils
 import android.util.Log
+import com.avos.avoscloud.AVException
 import com.avos.avoscloud.AVOSCloud
-import com.orhanobut.logger.Logger
+import com.avos.avoscloud.AVUser
+import com.avos.avoscloud.SignUpCallback
 import com.taobao.sophix.PatchStatus
 import com.taobao.sophix.SophixManager
-import com.taobao.sophix.listener.PatchLoadStatusListener
+import com.vm.shadowsocks.constant.Constant.TAG
 import com.vm.shadowsocks.domain.Server
-import java.io.InputStreamReader
+import com.vm.shadowsocks.tool.SystemUtil
+import com.vm.shadowsocks.tool.Tool
 
 
 /**
@@ -27,8 +29,46 @@ class App : MultiDexApplication() {
         super.onCreate()
         instance = this
         SophixManager.getInstance().queryAndLoadNewPatch();
+        AVOSCloud.initialize(this, "jadP41WoqD4mptx79gok48JY-gzGzoHsz", "VbupgDD0dyLX3pHuxgV8QAp7")
+        registerDevice()
+    }
 
-        AVOSCloud.initialize(this,"jadP41WoqD4mptx79gok48JY-gzGzoHsz","VbupgDD0dyLX3pHuxgV8QAp7")
+    fun registerDevice() {
+
+        try {
+            val address = Tool.getAdresseMAC(App.instance)
+            val ip = Tool.getLocalIpAddress()
+            val brand = SystemUtil.getDeviceBrand()
+            val model = SystemUtil.getSystemModel()
+            val imei = SystemUtil.getIMEI(this)
+
+            var avUser = AVUser()
+            avUser.username = SystemUtil.getDeviceBrand() + "," + SystemUtil.getIMEI(this) + "," + Tool.getAdresseMAC(this)
+            avUser.setPassword(""+avUser.hashCode())
+
+            avUser.put("mac", address)
+            avUser.put("ip", ip)
+            avUser.put("brand", "$brand,$model")
+            avUser.put("imei", imei)
+
+            avUser.put("system_version", Tool.getSystemVersion())
+            avUser.put("country", Tool.getCountryCode())
+            avUser.put("app_version", Tool.getVersionName(this))
+
+            avUser.signUpInBackground(object : SignUpCallback() {
+
+                override fun done(p0: AVException?) {
+                    if (null == p0) {
+                        Log.e(tag,"r-success")
+                    } else {
+                        Log.e(tag,"r-fail:"+p0?.localizedMessage)
+                    }
+                }
+            })
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 
     companion object {
@@ -49,11 +89,11 @@ class App : MultiDexApplication() {
                     // 补丁加载回调通知
                     if (code == PatchStatus.CODE_LOAD_SUCCESS) {
                         // 表明补丁加载成功
-                        Log.e(App.tag,"hot fix success======")
+                        Log.e(App.tag, "hot fix success======")
                     } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
                         // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
                         // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
-                        Log.e(App.tag,"hot fix success======restart")
+                        Log.e(App.tag, "hot fix success======restart")
                         SophixManager.getInstance().killProcessSafely()
                     } else {
                         // 其它错误信息, 查看PatchStatus类说明
